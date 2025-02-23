da75e29202066047a6aeac54299800276160f2fb
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-02-23 18:42:00 +0800

    Create show-onecompiler.js

---
+++ javascripts/discourse/components/show-onecompiler.js
@@ -0,0 +1,45 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { service } from "@ember/service";
+import { ajax } from "discourse/lib/ajax";
+import { popupAjaxError } from "discourse/lib/ajax-error";
+
+export default class ShowOneCompiler extends Component {
+  static hidden = true;
+  /*
+  @action
+  async showRaw() {
+    try {
+      const response = await ajax(`/posts/${this.args.post.id}/raw`, {
+        dataType: "text",
+      });
+      await this.modal.show(FullscreenCode, {
+        model: {
+          code: response,
+        },
+      });
+    } catch (e) {
+      popupAjaxError(e);
+    }
+  }
+  */
+  
+  <template>
+    <DButton
+      @translatedLabel="Show Modal"
+      @action={{fn (mut this.modalIsVisible) true}}
+    />
+
+    {{#if this.modalIsVisible}}
+      <DModal @title="Code Compiler" @closeModal={{fn (mut this.modalIsVisible) false}}>
+        <iframe
+         frameBorder="0"
+         height="450px"  
+         src="https://onecompiler.com/embed/python" 
+         width="100%"
+         >
+        </iframe>
+      </DModal>
+    {{/if}}
+  </template>
+}
