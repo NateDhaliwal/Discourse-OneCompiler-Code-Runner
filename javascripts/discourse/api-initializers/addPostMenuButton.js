d7e8b79c75b14fef6ed36103e501b00018ea327f
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-02-23 18:54:09 +0800

    Update addPostMenuButton.js

--- javascripts/discourse/api-initializers/addPostMenuButton.js
+++ javascripts/discourse/api-initializers/addPostMenuButton.js
@@ -1,7 +1,7 @@
 import { apiInitializer } from "discourse/lib/api";
 import ShowOnecompiler from "../components/show-onecompiler";
 
-export default apiInitializer("1.8.0", (/*api*/) => {
+export default apiInitializer("1.8.0", (api) => {
   api.registerValueTransformer(
     "post-menu-buttons",
     ({
